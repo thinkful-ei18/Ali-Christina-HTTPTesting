5183dc524c4e0ceb3bd084d538aadcce40aa799d
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-02-02 09:35:20 -0800

    Added POST test

--- test/test-recipies.js
+++ test/test-recipies.js
@@ -19,18 +19,33 @@ describe('test recipes', function() {
 
   it('should list recipes on GET', function() {
     return chai.request(app)
-    .get('/recipes')
-    .then(function(res) {
-      expect(res).to.have.status(200);
-      expect(res).to.be.json;
-      expect(res.body).to.be.a('array');
-      expect(res.body.length).to.be.at.least(1);
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
 
-      const expectedKeys = ['id', 'name', 'ingredients'];
-      res.body.forEach(function(recipe) {
-        expect(recipe).to.be.a('object');
-        expect(recipe).to.include.keys(expectedKeys);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(recipe) {
+          expect(recipe).to.be.a('object');
+          expect(recipe).to.include.keys(expectedKeys);
+        });
       });
-    });
+  });
+
+  it('should add recipe on POST', function() {
+    const newRecipe = {name: 'pizza', ingredients: ['sauce', 'dough', 'cheese']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+      });
   });
 });
